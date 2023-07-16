93ab87a4812cb6c3d91497a284ad28a5923ff27b
Amit Singh Rana <ranaa73000@gmail.com>
2023-07-16 13:17:37 +0530

    Setup logger, .env and DB

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import mongoose from "mongoose";
 import apiRoutes from "./routes/index.js";
 
 const app = express();
+
+// Enable/Process .env files
+dotenv.config();
+
+// Enable morgan logging
+app.use(morgan("tiny"));
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGOURI)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log("DB CONNECTION ERROR\n", err));
 
 // Attach API routes(Version 1)
 app.use("/api/v1", apiRoutes);
